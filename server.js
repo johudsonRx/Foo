6532066a4ff98b335c2c261844a516e5c7b5a1b6
Joshua Hudson <joshudson93@gmail.com>
2017-02-27 20:06:11 -0500

    Media Query stuff

--- server.js
+++ server.js
@@ -2,12 +2,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-
-
 // Create Instance of Express
 var app = express();
 var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
-
 // Run Morgan for Logging
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -20,9 +17,7 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
   res.sendFile(process.cwd() +'/index.html');
 })
-
 // -------------------------------------------------
-
 // Listener
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
